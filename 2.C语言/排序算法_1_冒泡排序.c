2fcff05b1f4bafd8523cd5aa3e6a9e41d0f6c635
epanll <epanll@outlook.com>
2023-09-26 19:51:26 +0800

    2023/9/26 添加排序算法:冒泡排序

---
+++ 2.C语言/排序算法_1_冒泡排序.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int bubble_sort(int *array, int size)
+{
+    int i, j, flag, temp;
+    for(i = size - 1; i >= 0; i--)
+    {
+        flag = 0;
+        for(j = 0; j < i; j++)
+        {
+            if(array[j] > array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                flag = 1;
+            }
+        }
+
+        if(flag == 0)
+            break;          //没有任何交换,说明顺序已经排好,跳出循环
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int buf[10] = {11, 0 ,8 , 7, 34, 99, 16, 22, 9, 55};
+    int size = sizeof(buf)/sizeof(int);
+    printf("before:");
+    for(int i = 0; i < size; i++)
+        printf(" %d ", buf[i]);
+    printf("\n");
+
+    bubble_sort(buf, size);
+    printf("_after:");
+    for(int i = 0; i < size; i++)
+        printf(" %d ", buf[i]);
+    printf("\n");
+
+    return 0;
+}
